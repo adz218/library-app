55f63265e0348314b40a6d738dca83009a2fb664
Adam Zhou <adzhou218@gmail.com>
2019-01-10 16:09:45 -0500

    reinstated welcome cmpt Jan102019

---
+++ client/components/welcome.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+const Welcome = props => (
+  <Fragment>
+    <p className="welcome-text">
+      Welcome! Begin searching for a book by title, author, or keyword above.
+    </p>
+  </Fragment>
+)
+
+export default Welcome
